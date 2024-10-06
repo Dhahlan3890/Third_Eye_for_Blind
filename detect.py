4ffe0645805454baad3f3c1b6894270193149bc0
Dhahlan3890 <dhahlan2000@gmail.com>
2024-10-06 14:19:25 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,5 +1,11 @@
 import cv2
 from ultralytics import YOLO
+import pyttsx3
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
 model = YOLO('best.pt')
 
@@ -12,6 +18,8 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 if not cap.isOpened():
     print("Error: Could not open video capture.")
     exit()
+
+print(model.names)
 
 while True:
     ret, frame = cap.read()
@@ -26,6 +34,8 @@ while True:
     confidences = results[0].boxes.conf.cpu().numpy()
     class_ids = results[0].boxes.cls.cpu().numpy()
 
+    positions = []
+
     for box, confidence, class_id in zip(boxes, confidences, class_ids):
         x1, y1, x2, y2 = map(int, box)
         class_id = int(class_id)
@@ -33,14 +43,46 @@ while True:
         color = (0, 255, 0)
         cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
         class_name = model.names[class_id]
+        
+        
         if (x1+x2)/2 > (2 * width)/3:
+            object_width = x2 - x1
             position = "right"
+            if object_width > 100:
+                object_width = "large"
+                positions.append(f"{position}_{object_width}")
+                speak(f"There is a {class_name} at {position} side")
+
         elif (x1+x2)/2 < (width)/3:
+            object_width = x2 - x1
             position = "left"
+            if object_width > 100:
+                object_width = "large"
+                positions.append(f"{position}_{object_width}")
+                speak(f"There is a {class_name} at {position} side")
+            
         else:
+            object_width = x2 - x1
             position = "center"
+            if object_width > 100:
+                object_width = "large"
+                positions.append(f"{position}{object_width}")
+                speak(f"There is a {class_name} infront of you careful")
+
         label = f"{class_name} ({confidence:.2f}) at {position}"
         cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    print(positions)
+    # if len(positions) > 0:
+    #     if "right_large" in positions:
+    #         print("Alert! There is a large object on your right.")
+    #         speak("Alert! Move to the left.")
+    #     elif "left_large" in positions:
+    #         print("Alert! There is a large object on your left.")
+    #         speak("Alert! Move to the right.") 
+    #     elif "center_large" in positions:
+    #         print("Alert! There is a large object in front of you.")
+    #         speak("Alert! There is a large object in front of you. Turn around.")
     
     cv2.imshow('Real-time Detection for blind people', frame)
     
